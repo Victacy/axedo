93337bbbffca4b0e1f303a461f1f122e286c4998
Xornam Adorkor <74544333+Victacy@users.noreply.github.com>
2023-07-03 19:25:31 +0100

    Filter added

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function addTodo(event) {
   let text = document.getElementById("create-todo");
   db.collection("todo-items").add({
     text: text.value,
-    rank: "live",
+    rank: "active",
   });
   text.value = "";
 }
@@ -26,14 +26,14 @@ function generateTodos(items) {
   let dosHTML = "";
   items.forEach((item) => {
     dosHTML += `
-        <div class="todo-text">
+        <div class="todo-text" draggable="true" data-id="${item.id}">
             <div class="mark">
-                <div data-id="${item.id}" class="marked ${item.rank == "fufilled" ? "checked":""}">
+                <div data-id="${item.id}" class="marked ${item.rank == "completed" ? "checked":""}">
                     <button  class="material-icons" >check</button>
                     <!-- <img src="./assets/check-icon.png" alt=""> -->
                 </div>
             </div>
-            <div class="text-item ${item.rank == "fufilled" ? "checked":""}">
+            <div class="text-item ${item.rank == "completed" ? "checked":""}">
                 ${item.text}
             </div>
             <div class="actions">
@@ -72,35 +72,135 @@ function actionListeners() {
           markDone(checker.dataset.id);
         }
       });
+      checker.addEventListener("dragstart", dragStart);
+      checker.addEventListener("dragover", dragOver);
+      checker.addEventListener("drop", drop);
     });
-
+    
+    // clear todo
     let clearBtn = document.querySelector(".clear-todos span");
     clearBtn.addEventListener("click", function () {
       clearCompletedTodos();
     });
+
+    // filter todo
+    let filterAll = document.getElementById("filter-all");
+    let filterActive = document.getElementById("filter-active");
+    let filterComplete = document.getElementById("filter-complete");
+  
+    filterAll.addEventListener("click", function () {
+      filterTodos("all");
+    });
+  
+    filterActive.addEventListener("click", function () {
+      filterTodos("active");
+    });
+  
+    filterComplete.addEventListener("click", function () {
+      filterTodos("completed");
+    });
   }
+
+
+  // Drag start event handler
+function dragStart(event) {
+  event.dataTransfer.setData("text/plain", event.target.dataset.id);
+}
+
+// Drag over event handler
+function dragOver(event) {
+  event.preventDefault();
+}
+
+// Drop event handler
+function drop(event) {
+  event.preventDefault();
+  const droppedItemId = event.dataTransfer.getData("text/plain");
+  const targetItemId = event.currentTarget.dataset.id;
+
+  // Swap the ranks of the dropped item and the target item
+  swapRanks(droppedItemId, targetItemId);
+}
+
+
+
+function filterTodos(filter) {
+  db.collection("todo-items")
+    .where("rank", "==", filter === "complete" ? "completed" : "acy=tive")
+    .onSnapshot((snapshot) => {
+      console.log(snapshot);
+      let items = [];
+      snapshot.docs.forEach((doc) => {
+        items.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      generateTodos(items);
+    });
+}
   
 
-function markDone(id){
-    let todo=db.collection("todo-items").doc(id)
+// function markDone(id){
+//     let todo=db.collection("todo-items").doc(id)
 
-    todo.get().then(function(doc){
-        if (doc.exists) {
-            let rank=doc.data().rank;
-            if(rank == "live"){
-                todo.update({
-                    rank:"fufilled"
-                })
-            }else if(rank == "fufilled"){
-                todo.update({
-                    rank:"live"
-                })
-            }
-        }
-    })
+//     todo.get().then(function(doc){
+//         if (doc.exists) {
+//             let rank=doc.data().rank;
+//             if(rank == "live"){
+//                 todo.update({
+//                     rank:"fufilled"
+//                 })
+//             }else if(rank == "fufilled"){
+//                 todo.update({
+//                     rank:"live"
+//                 })
+//             }
+//         }
+//     })
 
-    // console.log(id);
+//     // console.log(id);
+// }
+
+function markDone(id) {
+  let todo = db.collection("todo-items").doc(id);
+
+  todo.get().then(function (doc) {
+    if (doc.exists) {
+      let rank = doc.data().rank;
+      if (rank === "active") {
+        todo.update({
+          rank: "completed",
+        });
+      } else if (rank === "completed") {
+        todo.update({
+          rank: "active",
+        });
+      }
+    }
+  });
+  
+  // Call the swapRanks function
+  swapRanks(id, null); // passing null as the target ID indicates that it is the last item in the list
 }
+
+function swapRanks(droppedItemId, targetItemId) {
+  const todosRef = db.collection("todo-items");
+
+  todosRef.doc(droppedItemId).get().then((droppedItemDoc) => {
+    const droppedRank = droppedItemDoc.data().rank;
+    
+    // Update the dropped item's rank to the target item's rank
+    todosRef.doc(droppedItemId).update({ rank: targetItemId ? "active" : "completed" });
+
+    // Update the target item's rank to the dropped item's rank
+    if (targetItemId) {
+      todosRef.doc(targetItemId).update({ rank: droppedRank });
+    }
+  });
+}
+
+
 
 // delete todo
 function deleteTodo(id) {
@@ -130,5 +230,7 @@ function deleteTodo(id) {
       });
   }
   
+
+
 
 getTodos();
